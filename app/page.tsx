361cf36ffd28906a15f7afd53f0b50357b800eb7
Rejallejon <rufinrejallejon@gmail.com>
2026-02-04 12:38:59 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,84 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
 
-export default function Home() {
+export default function HomePage() {
+  const [noPos, setNoPos] = useState({ x: 0, y: 0 });
+  const [hasMoved, setHasMoved] = useState(false);
+  const [isYes, setIsYes] = useState(false);
+
+  const moveButton = () => {
+    const randomX = Math.random() * (window.innerWidth - 100);
+    const randomY = Math.random() * (window.innerHeight - 50);
+    setNoPos({ x: randomX, y: randomY });
+    setHasMoved(true);
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex justify-center items-center min-h-screen bg-pink-100 p-4">
+      <div className="w-full relative max-w-md bg-white p-8 rounded-xl shadow-lg ">
+        {isYes && (
+          <button
+            onClick={() => {
+              setIsYes(false);
+              setHasMoved(false);
+            }}
+            className="block text-start text-gray-500 mb-2 hover:text-black cursor-pointer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            ← Go back
+          </button>
+        )}
+        <video
+          key={isYes ? "happy-vid" : "ask-vid"}
+          src={
+            isYes
+              ? "/videos/Video_Generated_For_Minions.mp4"
+              : "/videos/Minions_Valentine_Request_Video.mp4"
+          }
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="rounded-lg w-full shadow-md"
+        >
+          Your browser does not support the video tag.
+        </video>
+
+        <h1 className="flex mt-8 justify-center items-center text-xl text-black font-bold min-h-15]">
+          {isYes
+            ? "Yeheeeeyyyyyy, I love you so much UWUUUUUUUU.."
+            : "Can you be my Valentine? Uwu"}
+          <FaHeart className="ml-2 text-red-500 text-2xl animate-pulse" />
+        </h1>
+
+        <div className="mt-6 flex justify-center items-center gap-4">
+          <button
+            onClick={() => setIsYes(true)}
+            className="p-4 bg-green-500 text-white rounded-lg px-8 font-bold hover:scale-110 transition-transform z-10"
           >
-            Documentation
-          </a>
+            YES
+          </button>
+
+          {!isYes && (
+            <button
+              onMouseEnter={moveButton}
+              style={
+                hasMoved
+                  ? {
+                      position: "fixed",
+                      left: `${noPos.x}px`,
+                      top: `${noPos.y}px`,
+                      zIndex: 999,
+                    }
+                  : { position: "relative" }
+              }
+              className="p-4 bg-red-500 text-white rounded-lg px-8 font-bold transition-all duration-150"
+            >
+              NO
+            </button>
+          )}
         </div>
-      </main>
+      </div>
     </div>
   );
 }
